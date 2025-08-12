8b29e5493175d32b5d4e04f1fe760f0ebb2b5ce9
Anna <2006apv@gmail.com>
2025-08-12 14:00:18 +0300

    Add new env variables to config

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,16 +2,40 @@ package config
 
 import (
 	"github.com/joho/godotenv"
+	"log"
 	"os"
+	"time"
 )
 
 type Config struct {
-	DBUrl string
+	DBUrl          string
+	APIPort        string
+	JWTSecret      string
+	JWTExpire      time.Duration
+	AuthService    string
+	UserService    string
+	GroupService   string
+	MemberService  string
+	ExpenseService string
 }
 
 func LoadConfig() *Config {
 	_ = godotenv.Load()
+
+	jwtExpire, err := time.ParseDuration(os.Getenv("JWT_EXPIRE"))
+	if err != nil {
+		log.Fatalf("Invalid JWT_EXPIRE: %v", err)
+	}
+
 	return &Config{
-		DBUrl: os.Getenv("DB_URL"),
+		DBUrl:          os.Getenv("DB_URL"),
+		APIPort:        os.Getenv("API_PORT"),
+		JWTSecret:      os.Getenv("JWT_SECRET"),
+		JWTExpire:      jwtExpire,
+		AuthService:    os.Getenv("AUTH_SERVICE_ADDR"),
+		UserService:    os.Getenv("USER_SERVICE_ADDR"),
+		GroupService:   os.Getenv("GROUP_SERVICE_ADDR"),
+		MemberService:  os.Getenv("MEMBER_SERVICE_ADDR"),
+		ExpenseService: os.Getenv("EXPENSE_SERVICE_ADDR"),
 	}
 }
